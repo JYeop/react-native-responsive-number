333f29300504a534b0f0bc4a9df3e489c5ccb0e5
JYK <husoo214@gmail.com>
2018-10-19 21:50:06 +0900

    first

---
+++ index.js
@@ -0,0 +1,82 @@
+import { Dimensions, Platform, StatusBar } from "react-native";
+
+const floorToInt = integer => {
+  return Math.floor(integer * 10) / 10;
+};
+const floorToFloat = integer => {
+  return Math.floor(integer);
+};
+
+export const dmFont = (integer, defaultWidth) => {
+  let widthToUse;
+  if (defaultWidth) {
+    widthToUse = defaultWidth;
+  } else {
+    widthToUse = 375;
+  }
+  const dvWidth = Dimensions.get("window").width;
+  const ratio = dvWidth / widthToUse;
+  return floorToInt(integer * ratio);
+};
+
+export const widthByPercent = stringInput => {
+  console.log(typeof stringInput === "number", typeof stringInput == "number");
+  const getRatio =
+    typeof stringInput === "number"
+      ? stringInput / 100
+      : Number(stringInput.replace("%", "")) / 100;
+  const dvWidth = Dimensions.get("window").width;
+  return floorToFloat(getRatio * dvWidth);
+};
+
+export const widthByRatio = stringInput => {
+  let getRatio;
+  if (typeof stringInput === "number") {
+    getRatio = stringInput;
+  } else {
+    getRatio = Number(stringInput);
+  }
+  const dvWidth = Dimensions.get("window").width;
+  return floorToFloat(getRatio * dvWidth);
+};
+
+export const heightByPercent = stringInput => {
+  let getRatio;
+  if (typeof stringInput === "number") {
+    getRatio = stringInput / 100;
+  } else {
+    getRatio = Number(stringInput.replace("%", "")) / 100;
+  }
+  const dvHeight = Dimensions.get("window").height;
+  return floorToFloat(getRatio * dvHeight);
+};
+
+export const heightByRatio = stringInput => {
+  let getRatio;
+  if (typeof stringInput === "number") {
+    getRatio = stringInput;
+  } else {
+    getRatio = Number(stringInput);
+  }
+  const dvHeight = Dimensions.get("window").height;
+  return floorToFloat(getRatio * dvHeight);
+};
+
+export const f = dmFont;
+export const wp = widthByPercent;
+export const wr = widthByRatio;
+export const hp = heightByPercent;
+export const hr = heightByRatio;
+
+export default {
+  dmFont,
+  widthByRatio,
+  heightByRatio,
+  widthByPercent,
+  heightByPercent,
+  f: dmFont,
+  wp: widthByPercent,
+  wr: widthByRatio,
+  hp: heightByPercent,
+  hr: heightByRatio
+};
